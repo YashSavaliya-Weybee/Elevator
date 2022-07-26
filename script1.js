0b5eabd34628d84ec6de2419032f09a6020a9058
YashSavaliya-Weybee <108047426+YashSavaliya-Weybee@users.noreply.github.com>
2022-07-26 10:58:57 +0530

    changes

--- script1.js
+++ script1.js
@@ -9,20 +9,24 @@ const elevator1 = document.getElementById('elevator1');
 const elevator2 = document.getElementById('elevator2');
 const elevator3 = document.getElementById('elevator3');
 const buttons = document.getElementsByTagName('button');
+let floreReqs;
 
 const allEle = [{
     id: 1,
     Top: 424,
+    oldTop: 424,
     isRunning: false,
     isOff: false
 }, {
     id: 2,
     Top: 424,
+    oldTop: 424,
     isRunning: false,
     isOff: false
 }, {
     id: 3,
     Top: 424,
+    oldTop: 424,
     isRunning: false,
     isOff: false
 }];
@@ -34,10 +38,38 @@ for (let i of buttons) {
     })
 }
 
+function offElevator(el) {
+    el.classList.toggle('onbtn');
+    const id = el.getAttribute('id');
+    switch (id[id.length - 1]) {
+        case '1':
+            elevator1.classList.contains('eleoff') ? elevator1.classList.remove('eleoff') : elevator1.classList.add('eleoff');
+            eleMove(424, elevator1, oldTop1);
+            elevator1.classList.contains('eleoff') ? allEle[0].isRunning = true : allEle[0].isRunning = false;
+            elevator1.classList.contains('eleoff') ? allEle[0].isOff = true : allEle[0].isOff = false;
+            elevator1.classList.contains('eleoff') ? allEle[0].Top = 425 : allEle[0].Top = 424;
+            break;
+        case '2':
+            elevator2.classList.contains('eleoff') ? elevator2.classList.remove('eleoff') : elevator2.classList.add('eleoff');
+            eleMove(424, elevator2, oldTop2);
+            elevator2.classList.contains('eleoff') ? allEle[1].isRunning = true : allEle[1].isRunning = false;
+            elevator2.classList.contains('eleoff') ? allEle[1].isOff = true : allEle[1].isOff = false;
+            elevator2.classList.contains('eleoff') ? allEle[1].Top = 425 : allEle[1].Top = 424;
+            break;
+        case '3':
+            elevator3.classList.contains('eleoff') ? elevator3.classList.remove('eleoff') : elevator3.classList.add('eleoff');
+            eleMove(424, elevator3, oldTop3);
+            elevator3.classList.contains('eleoff') ? allEle[2].isRunning = true : allEle[2].isRunning = false;
+            elevator3.classList.contains('eleoff') ? allEle[2].isOff = true : allEle[2].isOff = false;
+            elevator3.classList.contains('eleoff') ? allEle[2].Top = 425 : allEle[2].Top = 424;
+            break;
+    }
+}
+
 function chooseElevator(floreReqs) {
     switch (floreReqs) {
         case '1':
-            if (elevator1.style.top != '424px' && elevator2.style.top != '424px' && elevator3.style.top != '424px') {
+            if (allEle[0].Top != 424 && allEle[1].Top != 424 && allEle[2].Top != 424) {
                 const distOfAllEle = [];
                 allEle.forEach(ele => {
                     distOfAllEle.push(Math.abs(424 - ele.Top));
@@ -47,7 +79,7 @@ function chooseElevator(floreReqs) {
             }
             break;
         case '2':
-            if (elevator1.style.top != '318px' && elevator2.style.top != '318px' && elevator3.style.top != '318px') {
+            if (allEle[0].Top != 318 && allEle[1].Top != 318 && allEle[2].Top != 318) {
                 const distOfAllEle = [];
                 allEle.forEach(ele => {
                     distOfAllEle.push(Math.abs(318 - ele.Top));
@@ -57,7 +89,7 @@ function chooseElevator(floreReqs) {
             }
             break;
         case '3':
-            if (elevator1.style.top != '212px' && elevator2.style.top != '212px' && elevator3.style.top != '212px') {
+            if (allEle[0].Top != 212 && allEle[1].Top != 212 && allEle[2].Top != 212) {
                 const distOfAllEle = [];
                 allEle.forEach(ele => {
                     distOfAllEle.push(Math.abs(212 - ele.Top));
@@ -67,7 +99,7 @@ function chooseElevator(floreReqs) {
             }
             break;
         case '4':
-            if (elevator1.style.top != '106px' && elevator2.style.top != '106px' && elevator3.style.top != '106px') {
+            if (allEle[0].Top != 106 && allEle[1].Top != 106 && allEle[2].Top != 106) {
                 const distOfAllEle = [];
                 allEle.forEach(ele => {
                     distOfAllEle.push(Math.abs(106 - ele.Top));
@@ -77,7 +109,7 @@ function chooseElevator(floreReqs) {
             }
             break;
         case '5':
-            if (elevator1.style.top != '0px' && elevator2.style.top != '0px' && elevator3.style.top != '0px') {
+            if (allEle[0].Top != 0 && allEle[1].Top != 0 && allEle[2].Top != 0) {
                 const distOfAllEle = [];
                 allEle.forEach(ele => {
                     distOfAllEle.push(Math.abs(0 - ele.Top));
@@ -90,35 +122,69 @@ function chooseElevator(floreReqs) {
 }
 
 function allindex(minDis, distOfAllEle) {
+    debugger
     let occuranceOfMinInd = [0, 0, 0];
-    for (let i = 0; i < distOfAllEle.length; i++) {
+    for (let i = 0; i < 3; i++) {
         if (minDis == distOfAllEle[i]) {
             occuranceOfMinInd[i] = 1;
         }
     }
 
     if (occuranceOfMinInd[0] == 1 && occuranceOfMinInd[1] == 1 && occuranceOfMinInd[2] == 1) {
-        let randEle = Math.trunc(Math.random() * occuranceOfMinInd.length + 1);
-        randEle == 1 ? (allEle[0].isRunning ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator1, oldTop1)) :
-            randEle == 2 ? (allEle[1].isRunning ? travelElevator(floreReqs, elevator3, oldTop3) : travelElevator(floreReqs, elevator2, oldTop2)) :
-                (ele3isTravallEle[2].isRunningeling ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator3, oldTop3))
+        if (!allEle[0].isRunning && !allEle[1].isRunning && !allEle[2].isRunning) {
+            let randEle = Math.trunc(Math.random() * 3 + 1);
+            randEle == 1 ? travelElevator(floreReqs, elevator1, oldTop1) : randEle == 2 ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator3, oldTop3);
+        } else {
+            nearestEle(...distOfAllEle)
+        }
     } else {
         if (occuranceOfMinInd[0] == 1 && occuranceOfMinInd[1] == 1) {
-            let randEle = Math.trunc(Math.random() * 2 + 1);
-            randEle == 1 ? (allEle[0].isRunning ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator1, oldTop1)) :
-                (allEle[1].isRunning ? travelElevator(floreReqs, elevator1, oldTop1) : travelElevator(floreReqs, elevator2, oldTop2));
+            if (allEle[0].isRunning && allEle[1].isRunning) {
+                travelElevator(floreReqs, elevator3, oldTop3)
+            } else {
+                let randEle = Math.trunc(Math.random() * 2 + 1);
+                randEle == 1 ? (allEle[0].isRunning ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator1, oldTop1)) :
+                    (allEle[1].isRunning ? travelElevator(floreReqs, elevator1, oldTop1) : travelElevator(floreReqs, elevator2, oldTop2));
+            }
         } else if (occuranceOfMinInd[1] == 1 && occuranceOfMinInd[2] == 1) {
-            let randEle = Math.trunc(Math.random() * 2 + 1);
-            randEle == 1 ? (allEle[1].isRunning ? travelElevator(floreReqs, elevator3, oldTop3) : travelElevator(floreReqs, elevator2, oldTop2)) :
-                (allEle[2].isRunning ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator3, oldTop3));
+            if (allEle[1].isRunning && allEle[2].isRunning) {
+                travelElevator(floreReqs, elevator1, oldTop1)
+            } else {
+                let randEle = Math.trunc(Math.random() * 2 + 1);
+                randEle == 1 ? (allEle[1].isRunning ? travelElevator(floreReqs, elevator3, oldTop3) : travelElevator(floreReqs, elevator2, oldTop2)) :
+                    (allEle[2].isRunning ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator3, oldTop3));
+            }
         } else if (occuranceOfMinInd[0] == 1 && occuranceOfMinInd[2] == 1) {
-            let randEle = Math.trunc(Math.random() * 2 + 1);
-            randEle == 1 ? (allEle[0].isRunning ? travelElevator(floreReqs, elevator3, oldTop3) : travelElevator(floreReqs, elevator1, oldTop1)) :
-                (allEle[2].isRunning ? travelElevator(floreReqs, elevator1, oldTop1) : travelElevator(floreReqs, elevator3, oldTop3));
+            if (allEle[0].isRunning && allEle[2].isRunning) {
+                travelElevator(floreReqs, elevator2, oldTop2)
+            } else {
+                let randEle = Math.trunc(Math.random() * 2 + 1);
+                randEle == 1 ? (allEle[0].isRunning ? travelElevator(floreReqs, elevator3, oldTop3) : travelElevator(floreReqs, elevator1, oldTop1)) :
+                    (allEle[2].isRunning ? travelElevator(floreReqs, elevator1, oldTop1) : travelElevator(floreReqs, elevator3, oldTop3));
+            }
         } else {
-            occuranceOfMinInd[0] == 1 ? (allEle[0].isRunning ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator1, oldTop1)) :
-                occuranceOfMinInd[1] == 1 ? (allEle[1].isRunning ? travelElevator(floreReqs, elevator3, oldTop3) : travelElevator(floreReqs, elevator2, oldTop2)) :
-                    (allEle[2].isRunning ? travelElevator(floreReqs, elevator1, oldTop1) : travelElevator(floreReqs, elevator3, oldTop3))
+            occuranceOfMinInd[0] == 1 ? (allEle[0].isRunning ? nearestEle(distOfAllEle.slice(1, 3)) : travelElevator(floreReqs, elevator1, oldTop1)) :
+                occuranceOfMinInd[1] == 1 ? (allEle[1].isRunning ? nearestEle([distOfAllEle[0], distOfAllEle[2]]) : travelElevator(floreReqs, elevator2, oldTop2)) :
+                    (allEle[2].isRunning ? nearestEle(distOfAllEle.slice(0, 2)) : travelElevator(floreReqs, elevator3, oldTop3))
+        }
+    }
+}
+
+function nearestEle(...distOfAllEle) {
+    let minDisIndex = distOfAllEle.indexOf(Math.min(...distOfAllEle));
+    if (allEle[0].isRunning && allEle[1].isRunning) {
+        travelElevator(floreReqs, elevator3, oldTop3);
+    } else if (allEle[1].isRunning && allEle[2].isRunning) {
+        travelElevator(floreReqs, elevator1, oldTop1);
+    } else if (allEle[0].isRunning && allEle[2].isRunning) {
+        travelElevator(floreReqs, elevator2, oldTop2);
+    } else {
+        if (allEle[0].isRunning) {
+            minDisIndex == 0 ? travelElevator(floreReqs, elevator2, oldTop2) : travelElevator(floreReqs, elevator3, oldTop3);
+        } else if (allEle[1].isRunning) {
+            minDisIndex == 0 ? travelElevator(floreReqs, elevator1, oldTop1) : travelElevator(floreReqs, elevator3, oldTop3);
+        } else if (allEle[2].isRunning) {
+            minDisIndex == 0 ? travelElevator(floreReqs, elevator1, oldTop1) : travelElevator(floreReqs, elevator2, oldTop2);
         }
     }
 }
@@ -145,12 +211,13 @@ function travelElevator(floreReqs, ele, oldtop) {
 
 function eleMove(top, eleNo, oldtop) {
     const ele = eleNo.getAttribute('id');
+
     if (ele == 'elevator1') {
         if (!allEle[0].isOff) {
             if (!allEle[0].isRunning) {
                 allEle[0].isRunning = true;
                 allEle[0].Top = top;
-                if (oldtop < top) {
+                if (oldtop <= top) {
                     clearInterval(id1);
                     id1 = setInterval(frame, 0);
                     function frame() {
@@ -158,6 +225,8 @@ function eleMove(top, eleNo, oldtop) {
                             clearInterval(id1);
                             allEle[0].isRunning = false;
                             elevator1.classList.contains('eleoff') ? allEle[0].isOff = true : allEle[0].isOff = false;
+                            elevator1.classList.contains('eleoff') ? allEle[0].isRunning = true : allEle[0].isRunning = false;
+                            elevator1.classList.contains('eleoff') ? allEle[0].Top = 425 : undefined;
                         } else {
                             oldtop++;
                             eleNo.style.top = oldtop + 'px';
@@ -168,11 +237,14 @@ function eleMove(top, eleNo, oldtop) {
                 } else if (oldtop > top) {
                     clearInterval(id1);
                     id1 = setInterval(frame, 0);
+
                     function frame() {
                         if (oldtop == top) {
                             clearInterval(id1);
                             allEle[0].isRunning = false;
                             elevator1.classList.contains('eleoff') ? allEle[0].isOff = true : allEle[0].isOff = false;
+                            elevator1.classList.contains('eleoff') ? allEle[0].isRunning = true : allEle[0].isRunning = false;
+                            elevator1.classList.contains('eleoff') ? allEle[0].Top = 425 : undefined;
                         } else {
                             oldtop--;
                             eleNo.style.top = oldtop + 'px';
@@ -188,7 +260,7 @@ function eleMove(top, eleNo, oldtop) {
             if (allEle[1].isRunning == false) {
                 allEle[1].isRunning = true;
                 allEle[1].Top = top;
-                if (oldtop < top) {
+                if (oldtop <= top) {
                     clearInterval(id2);
                     id2 = setInterval(frame, 0);
                     function frame() {
@@ -196,6 +268,8 @@ function eleMove(top, eleNo, oldtop) {
                             clearInterval(id2);
                             allEle[1].isRunning = false;
                             elevator2.classList.contains('eleoff') ? allEle[1].isOff = true : allEle[1].isOff = false;
+                            elevator2.classList.contains('eleoff') ? allEle[1].isRunning = true : allEle[1].isRunning = false;
+                            elevator2.classList.contains('eleoff') ? allEle[1].Top = 425 : undefined;
                         } else {
                             oldtop++;
                             eleNo.style.top = oldtop + 'px';
@@ -206,11 +280,14 @@ function eleMove(top, eleNo, oldtop) {
                 } else if (oldtop > top) {
                     clearInterval(id2);
                     id2 = setInterval(frame, 0);
+
                     function frame() {
                         if (oldtop == top) {
                             clearInterval(id2);
                             allEle[1].isRunning = false;
                             elevator2.classList.contains('eleoff') ? allEle[1].isRunning = true : allEle[1].isRunning = false;
+                            elevator2.classList.contains('eleoff') ? allEle[1].isRunning = true : allEle[1].isRunning = false;
+                            elevator2.classList.contains('eleoff') ? allEle[1].Top = 425 : undefined;
                         } else {
                             oldtop--;
                             eleNo.style.top = oldtop + 'px';
@@ -226,7 +303,7 @@ function eleMove(top, eleNo, oldtop) {
             if (allEle[2].isRunning == false) {
                 allEle[2].isRunning = true;
                 allEle[2].Top = top;
-                if (oldtop < top) {
+                if (oldtop <= top) {
                     clearInterval(id3);
                     id3 = setInterval(frame, -100);
                     function frame() {
@@ -234,6 +311,8 @@ function eleMove(top, eleNo, oldtop) {
                             clearInterval(id3);
                             allEle[2].isRunning = false;
                             elevator3.classList.contains('eleoff') ? allEle[2].isOff = true : allEle[2].isOff = false;
+                            elevator3.classList.contains('eleoff') ? allEle[2].isRunning = true : allEle[2].isRunning = false;
+                            elevator3.classList.contains('eleoff') ? allEle[2].Top = 425 : undefined;
                         } else {
                             oldtop++;
                             eleNo.style.top = oldtop + 'px';
@@ -244,11 +323,14 @@ function eleMove(top, eleNo, oldtop) {
                 } else if (oldtop > top) {
                     clearInterval(id3);
                     id3 = setInterval(frame, -100);
+
                     function frame() {
                         if (oldtop == top) {
                             clearInterval(id3);
                             allEle[2].isRunning = false;
                             elevator3.classList.contains('eleoff') ? allEle[2].isOff = true : allEle[2].isOff = false;
+                            elevator3.classList.contains('eleoff') ? allEle[2].isRunning = true : allEle[2].isRunning = false;
+                            elevator3.classList.contains('eleoff') ? allEle[2].Top = 425 : undefined;
                         } else {
                             oldtop--;
                             eleNo.style.top = oldtop + 'px';
@@ -267,67 +349,5 @@ function floreNumber(ele) {
         ele.style.top == '106px' ? ele.textContent = 4 :
             ele.style.top == '212px' ? ele.textContent = 3 :
                 ele.style.top == '318px' ? ele.textContent = 2 :
-                    ele.style.top == '424px' ? ele.textContent = 1 : console.log('')
+                    ele.style.top == '424px' ? ele.textContent = 1 : console.log();
 }
-
-
-
-
-
-// // oprateBtn.forEach(ele => {
-// //     ele.addEventListener("click", function (e) {
-// //         e.preventDefault();
-// //         console.log("id", ele.id);
-// //         const element = compareLocation(ele.id.slice(1));
-// //         moveElevator(ele.id.slice(1), element);
-// //     })
-// // });
-
-// function compareLocation(flore) {
-//     const newTop = (oldTop - (80 * (flore - 1)));
-
-//     const moveleep = [];
-//     dataArr.forEach(ele => {
-//         const topNew = ele.toper.split("").slice(0, -2).join("");
-//         const difference = Math.abs(newTop - (+topNew));
-//         moveleep.push(difference);
-//     });
-
-//     let minimum = Math.min(...moveleep);
-
-//     const index = moveleep.indexOf(minimum);
-
-//     // const elementOf = dataArr[index].element;
-
-//     dataArr[index].isRunnig = true;
-//     return dataArr[index].element;
-
-
-// }
-
-// function moveElevator(flore, elevator) {
-//     const newTop = (oldTop - (80 * (flore - 1))) + 'px';
-//     const newEle = dataArr.find(ele => ele.element == elevator);
-//     // const newElement = newEle.element;
-//     newEle.element.style.top = newTop;
-//     newEle.toper = newTop;
-//     newEle.element.style.transition = "all 1s linear";
-
-//     let count = 0;
-//     lip.forEach(ele => {
-//         if (ele.children[0].style.top == newTop) {
-//             count = 1;
-//         }
-//     });
-
-//     if (count == 0) {
-//         for (let i = 1; i <= answer; i++) {
-//             if (flore == i) {
-//                 elevator.style.top = newTop;
-//                 elevator.style.transition = 'all 0.5s linear';
-//                 break;
-//             }
-//         }
-//     }
-
-// }
